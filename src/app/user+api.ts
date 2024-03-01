7315f80aa8030de503e2393214102dfaf4aed923
gustavogss <gustavogss.dev@gmail.com>
2024-03-01 00:07:22 -0300

    feat: get api

--- src/app/user+api.ts
+++ src/app/user+api.ts
@@ -1,1 +1,8 @@
 
+import {ExpoResponse} from 'expo-router/server';
+
+export async function GET(): Promise<ExpoResponse>{
+    return ExpoResponse.json({
+        message: 'Resposta do banck-end'
+    })
+}
