4e8f6a9f3cd619e4783e512e1dd700f6cee87829
gustavogss <gustavogss.dev@gmail.com>
2024-03-01 00:08:14 -0300

    feat: post api

--- src/app/user+api.ts
+++ src/app/user+api.ts
@@ -1,8 +1,16 @@
-
-import {ExpoResponse} from 'expo-router/server';
+import {ExpoResponse, ExpoRequest} from 'expo-router/server';
 
 export async function GET(): Promise<ExpoResponse>{
     return ExpoResponse.json({
         message: 'Resposta do banck-end'
+    })
+}
+
+export async function POST(request: ExpoRequest): Promise<ExpoResponse>{
+    const {username, password} = await request.json()
+    
+    return ExpoResponse.json({
+        username,
+        password
     })
 }
